419df697a448ce142f6ca26862204de5a76b9d1a
Tran Bac Son <tbosn87@gmail.com>
2018-04-28 18:28:25 +0700

    remove fontawesome dependencies

--- client/src/utils/components/RichTextInput.js
+++ client/src/utils/components/RichTextInput.js
@@ -101,6 +101,10 @@ const styles = {};
 export default RichTextInput;
 
 class CustomImageSideButton extends ImageSideButton {
+    constructor(props) {
+        super(props);
+    }
+
     async onChange(e) {
         const file = e.target.files[0];
         if (file.type.indexOf('image/') === 0) {
@@ -120,5 +124,22 @@ class CustomImageSideButton extends ImageSideButton {
             }
         }
         this.props.close();
+    }
+
+    render() {
+        return (
+            <button className="md-sb-button md-sb-img-button" type="button" onClick={this.onClick} title="Add an Image">
+                <span className="oi oi-image" style={{fontSize: 15}}></span>
+                <input
+                    type="file"
+                    accept="image/*"
+                    ref={c => {
+                        this.input = c;
+                    }}
+                    onChange={this.onChange}
+                    style={{display: 'none'}}
+                />
+            </button>
+        );
     }
 }
