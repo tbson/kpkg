9e22171aa78b33961439bc537839508c9cee474d
Tran Bac Son <tbosn87@gmail.com>
2018-04-28 17:42:41 +0700

    ensure next/previous link in https

--- api/utils/common_classes/custom_pagination.py
+++ api/utils/common_classes/custom_pagination.py
@@ -1,15 +1,25 @@
 from rest_framework import pagination
 from rest_framework.response import Response
 from math import ceil
+from django.conf import settings
 
 
 class CustomPagination(pagination.PageNumberPagination):
 
     def get_paginated_response(self, data):
+        next_link = self.get_next_link()
+        previous_link = self.get_previous_link()
+
+        if settings.PROTOCOL == 'https':
+            if next_link:
+                next_link = next_link.replace('http://', 'https://')
+            if previous_link:
+                previous_link = previous_link.replace('http://', 'https://')
+
         return Response({
             'links': {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link()
+                'next': next_link,
+                'previous': previous_link
             },
             'count': self.page.paginator.count,
             'pages': ceil(self.page.paginator.count / self.page_size),
