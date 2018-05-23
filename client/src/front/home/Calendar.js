9f825875808f8238e6718f18e4b1af4714631d7b
Tran Bac Son <tbosn87@gmail.com>
2018-05-23 13:44:28 +0700

    prepare to render otherEvents section

--- client/src/front/home/Calendar.js
+++ client/src/front/home/Calendar.js
@@ -12,33 +12,50 @@ import Carousel from 'src/utils/components/Carousel';
 import Tools from 'src/utils/helpers/Tools';
 import {apiUrls} from '../common/_data';
 import type {FormValues as EventType} from 'src/back/ccalendar/_data';
+import type {FormValues as ArticleType} from 'src/back/article/_data';
 
 type Props = {};
 type State = {
     events: Array<EventType>,
-    dataLoaded: boolean,
+    otherEvents: Array<ArticleType>,
+    calendarLoaded: boolean,
+    otherEventsLoaded: boolean,
 };
 
 export default class Calendar extends React.Component<Props, State> {
     static defaultProps = {};
     state: State = {
         events: [],
-        dataLoaded: false,
+        otherEvents: [],
+        calendarLoaded: false,
+        otherEventsLoaded: false,
     };
     constructor(props: Props) {
         super(props);
+        BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
     }
 
     componentDidMount() {
         const events = Tools.getGlobalState('events');
+        const otherEvents = Tools.getGlobalState('events');
 
         if (events) {
             return this.setState({
                 events,
-                dataLoaded: true,
+                calendarLoaded: true,
             });
+        } else {
+            this.getCalendar();
         }
-        this.getCalendar();
+
+        if (otherEvents) {
+            return this.setState({
+                otherEvents,
+                otherEventsLoaded: true,
+            });
+        } else {
+            this.getOtherEvents();
+        }
     }
 
     getCalendar = async () => {
@@ -46,22 +63,35 @@ export default class Calendar extends React.Component<Props, State> {
         if (result.success) {
             this.setState({
                 events: result.data.items,
-                dataLoaded: true,
+                calendarLoaded: true,
             });
             Tools.setGlobalState('events', result.data.items);
         }
     };
+
+    getOtherEvents = async () => {
+        const result = await Tools.apiCall(
+            apiUrls.homeArticle + 'category__uid=su-kien-thien-van?limit=5', 'GET',
+            {}, false, false
+        );
+        if (result.success) {
+            this.setState({
+                otherEvents: result.data.items,
+                otherEventsLoaded: true,
+            });
+            Tools.setGlobalState('other_events', result.data.items);
+        }
+    }
 
     selectDateHandle = (event: Object) => {
         if (!event.url) return;
         window.open(event.url, '_blank');
     };
 
-    render() {
-        if (!this.state.dataLoaded) return null;
-        BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
+    renderCalendar = () => {
+        if (!this.state.calendarLoaded) return null;
         return (
-            <React.Fragment>
+            <div>
                 <div className="content-container">
                     <h1>Lịch Thiên Văn</h1>
                 </div>
@@ -77,6 +107,14 @@ export default class Calendar extends React.Component<Props, State> {
                         />
                     </div>
                 </div>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                {this.renderCalendar()}
             </React.Fragment>
         );
     }
